bca462db0d11ad4fcd5421b00a93b49fbc3ce5c9
Alexandre Ribeiro de Oliveira <alexandre.programador84@gmail.com>
2022-04-18 14:46:57 -0300

    Adicionar arquivos de projeto.

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Singleton
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Padrão de Projeto Singleton");
+
+            Singleton jog1 = Singleton.GetInstancia;
+            jog1.Mensagem("Bola com o jogador 1.");
+
+            Singleton jog2 = Singleton.GetInstancia;
+            jog2.Mensagem("Bola com o jogador 2");
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Singleton.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Singleton
+{
+    public sealed class Singleton
+    {
+        private static Singleton instancia = null;
+        public static Singleton GetInstancia
+        {
+            get
+            {
+                if (instancia == null)
+                {
+                    instancia = new Singleton();
+                    Console.WriteLine("Objeto Instânciado!");
+                }
+
+                return instancia;
+            }
+        }
+
+        public void Mensagem(string mensagem)
+        {
+            Console.WriteLine(mensagem);
+        }
+    }
+}
